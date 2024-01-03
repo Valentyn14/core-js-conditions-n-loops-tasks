b16b76e44b2cd1fb87899ea11381bcc20f4df7ec
Valentyn Lysenko <valentynlysenco@gmail.com>
2024-01-03 21:21:01 +0200

    feat: Solve the function getMaxNumber

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -42,8 +42,15 @@ function isPositive(number) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  let res = a;
+  if (b > a && b > c) {
+    res = b;
+  } else if (c > a && c > b) {
+    res = c;
+  }
+
+  return res;
 }
 
 /**
